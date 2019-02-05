65704fcf82d163d0484ce5398a6e9a4f45411f60
tanabenobuyuki <tanabe.1478@gmail.com>
2019-02-05 11:24:22 +0900

    feat: RepositoryのfullNameプロパティをsnake caseに対応

--- Request/Repository.swift
+++ Request/Repository.swift
@@ -12,4 +12,11 @@ struct Repository: Decodable {
     let name: String
     let fullName: String
     let owner: User
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case fullName = "full_name"
+        case owner
+    }
 }
