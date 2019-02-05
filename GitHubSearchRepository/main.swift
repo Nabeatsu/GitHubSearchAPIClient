07e05322e1adf295d980828ad5662f6f439bc878
tanabenobuyuki <tanabe.1478@gmail.com>
2019-02-05 11:17:14 +0900

    Initial Commit

---
+++ GitHubSearchRepository/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GitHubSearchRepository
+//
+//  Created by 田辺信之 on 2019/02/05.
+//  Copyright © 2019 田辺信之. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
