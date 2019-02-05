f9d39195f5967b5243bb2453a305fc008f1e0a2a
tanabenobuyuki <tanabe.1478@gmail.com>
2019-02-05 12:01:33 +0900

    feat: リクエストに必要なbaseURLとHTTPMethodを定義

---
+++ Request/GitHubRequest.swift
@@ -0,0 +1,29 @@
+//
+//  GitHubRequest.swift
+//  GitHubSearchRepository
+//
+//  Created by 田辺信之 on 2019/02/05.
+//  Copyright © 2019 田辺信之. All rights reserved.
+//
+
+import Foundation
+
+
+// ベースURLとパスの定義
+// GitHub APIのURLには共通の部分が多くあるため、URLをベースURLとパスの2つのプロパティにわけて、
+// GitHubRequesプロトコルに定義する
+
+protocol GitHubRequest {
+    var baseURL: URL { get }
+    var path: String { get }
+    var method: HTTPMethod { get }
+}
+
+
+extension GitHubRequest {
+    var baseURL: URL {
+        return URL(string: "https://api.github.com")!
+    }
+}
+
+
---
+++ Request/HTTPMethod.swift
@@ -0,0 +1,22 @@
+//
+//  HTTPMethod.swift
+//  GitHubSearchRepository
+//
+//  Created by 田辺信之 on 2019/02/05.
+//  Copyright © 2019 田辺信之. All rights reserved.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case head = "HEAD"
+    case delete = "DELETE"
+    case patch = "PATCH"
+    case trace = "TRACE"
+    case options = "OPTIONS"
+    case connect = "CONNECT"
+}
+
